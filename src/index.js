b5916e57b76fbd9c4e1dfe4f7a1b5cf15fee41d2
ginipiga <stargling0285@gmail.com>
2022-05-18 16:10:20 +0900

    D-Day_Calculator
    크리스마스까지 며칠 남았는지 세주는 것

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-const GenNum = document.querySelector(".GenerateNum input");
-const GueNum = document.querySelector(".GuessNum input");
-const Maxvalue=GenNum.value;
-const GBtn = document.querySelector(".GuessNum button");
-const Youchose=document.querySelector(".YouChose");
-const Result=document.querySelector(".result");
-function maxvalue(event){
-    event.preventDefault();
-    const Guevalue = GueNum.value;
-    const Maxvalue=GenNum.value;
-    if(Guevalue <= 0){
-        alert("양수 정수값을 입력하시오.");
-    }
-    else if(Maxvalue < Guevalue){
-        alert(`${Maxvalue} 보다 작은값을 입력하시오.`);
-    }
-    else{
-        GueNum.setAttribute('max', Maxvalue);
-    }
-}
+const clockTitle = document.querySelector(".js-clock");
 
-function GamePlay(){
-    const GueValue = GueNum.value;
-    const Maxvalue=GenNum.value;
-    const randomValue = Math.ceil(Math.random()*(Maxvalue));
-    Youchose.innerText=`You chose: ${GueValue}, the machine chose: ${randomValue}`;
-    if(GueValue == randomValue){
-        Result.innerText="You won!"
-    }else{
-        Result.innerText="You lost!"
-    }
-}
-GueNum.addEventListener("input",maxvalue);
-GBtn.addEventListener("click",GamePlay);
+function C_Day() {
+    const masTime = new Date("2022-12-25, 0:00:00");
+    const todayTime = new Date();
+    const diff = masTime - todayTime;
+    const diffDay = String(Math.floor(diff / (1000*60*60*24)));
+    const diffHour =String( Math.floor((diff / (1000*60*60)) % 24)).padStart(2,"0");
+    const diffMin = String(Math.floor((diff / (1000*60)) % 60)).padStart(2,"0");
+    const diffSec = String(Math.floor(diff / 1000 % 60)).padStart(2,"0");
+    clockTitle.innerText = `${diffDay}d ${diffHour}h ${diffMin}m ${diffSec}s`; }
+    
+C_Day();
+setInterval(C_Day, 1000);
