1d9fa31b7f0779ad8cbd85235348c2a1a5f2f818
ginipiga <stargling0285@gmail.com>
2022-05-19 15:31:19 +0900

    addevent click change background
    클릭을 하면 그라데이션, 백 그라운드가 랜덤적으로 나오게 하기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const clockTitle = document.querySelector(".js-clock");
-
-function C_Day() {
-    const masTime = new Date("2022-12-25, 0:00:00");
-    const todayTime = new Date();
-    const diff = masTime - todayTime;
-    const diffDay = String(Math.floor(diff / (1000*60*60*24)));
-    const diffHour =String( Math.floor((diff / (1000*60*60)) % 24)).padStart(2,"0");
-    const diffMin = String(Math.floor((diff / (1000*60)) % 60)).padStart(2,"0");
-    const diffSec = String(Math.floor(diff / 1000 % 60)).padStart(2,"0");
-    clockTitle.innerText = `${diffDay}d ${diffHour}h ${diffMin}m ${diffSec}s`; }
-    
-C_Day();
-setInterval(C_Day, 1000);
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+const button = document.querySelector("button");
+const backcolor = document.querySelector("body");
+function colorChange(){
+    const colors1 = colors[Math.floor(Math.random()*colors.length)];
+    const colors2 = colors[Math.floor(Math.random()*colors.length)];
+    backcolor.style.backgroundImage = `linear-gradient(90deg, ${colors1}, ${colors2})`;
+}
+backcolor.addEventListener("click", colorChange);
